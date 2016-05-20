06d5202180ac87b16c0add027a2daa2a743faa34
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2016-05-20 12:49:02 +0900

    commit

--- api/shop.js
+++ api/shop.js
@@ -4,10 +4,17 @@
 import _products from './products.json'
 
 const TIMEOUT = 100
+// HEROKUで動かす用
+const PRODUCTS = [
+  {"id": 1, "title": "iPad 4 Mini", "price": 500.01, "inventory": 2},
+  {"id": 2, "title": "H&M T-Shirt White", "price": 10.99, "inventory": 10},
+  {"id": 3, "title": "Charli XCX - Sucker CD", "price": 19.99, "inventory": 5}
+]
 
 export default {
   getProducts(cb, timeout) {
-    setTimeout(() => cb(_products), timeout || TIMEOUT)
+    //setTimeout(() => cb(_products), timeout || TIMEOUT) <- Herokuでは動かない
+    setTimeout(() => cb(PRODUCTS), timeout || TIMEOUT)
   },
 
   buyProducts(payload, cb, timeout) {
